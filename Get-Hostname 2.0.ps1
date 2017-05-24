cdffc7d9b2f2a8981c293b4e416a6dd044e89780
Connor LaFleur <clafleur@main.city.northampton.ma.us>
2017-05-24 14:26:56 -0400

    Nearly Completed Get-IPs. Still having problems with 2-digit host portions of IPs. Began setting up Get-Hostname.

--- Get-Hostname 2.0.ps1
+++ Get-Hostname 2.0.ps1
@@ -1,18 +1,23 @@
 function Get-Hostname {
+  Param(
+
+#Taking these values as pipleine input from Get-IPs saves a great deal of logic and processing time.
+#I will not implement manual specification of IPs and subnets as there is no reason to attempt to return hostnames from unassigned IPs.
+#If for some reason you feel the need to implement this feature all the logic from Get-IPs should work here with small modifications.
+
   [Parameter(Mandatory = $True,
-             Position =0,
              ValueFromPipeline = $True,
              ValueFromPipelineByPropertyName = $True,
-             HelpMessage ="Specify the first IP in the range(s).")]
-  [Alias('Start')]
-  [String] $StartIP,
+             HelpMessage ="This is intended to receive pipeline input from Get-IPs.")]
+  [String[]] $IPRange,
 
-  [Parameter(Mandatory = $True,
-             Position =1,
-             HelpMessage ="Enter the last IP in the range(s).")]
-  [Alias('End')]
-  [String] $EndIP
-
-    foreach($IP in $IPRange)
+[Parameter(Mandatory = $False,
+             ValueFromPipeline = $True,
+             ValueFromPipelineByPropertyName = $True,
+             HelpMessage ="Specify the subnets you wish to get hostames from (This can be a list passed from Get-IPs).")]
+  [String[]] $Multiple
+)
+    foreach($IP in $IPRange){
     [System.Net.DNS]::GetHostEntry("$IP")
+  }
 }
--- Get-IPs.ps1
+++ Get-IPs.ps1
@@ -1,12 +1,13 @@
 function Get-IPs {
-
+    
     [cmdletbinding()]
     Param(
+
         [Parameter(Mandatory = $True,
                    Position =0,
                    ValueFromPipeline = $True,
                    ValueFromPipelineByPropertyName = $True,
-                   HelpMessage ="Enter the first IP in the range(s).")]
+                   HelpMessage ="Specify the first IP in the range(s).")]
         [Alias('Start')]
         [String] $StartIP,
 
@@ -14,34 +15,40 @@ function Get-IPs {
                    Position =1,
                    HelpMessage ="Enter the last IP in the range(s).")]
         [Alias('End')]
-        [String] $EndIP
+        [String] $EndIP,
 
-        [Parameter(Mandatory = $False
-                  HelpMessage ="Enter the first three octets in the first subnet.")]
-        $          
+        [Parameter(Mandatory = $False,
+                  HelpMessage ="Enter the subnets you want to scan.")]
+        
+        #This is an array of strings intended to store as many subnets as the user wishes. It is named Multiple so it is clear as a parameter.
+        [String[]] $Multiple 
     )
-    [Int]$BitCount = 0
-    [String]$Subnet
+    [Int] $BitCount = 0
+    [String] $Subnet
 
-    for([Int] $i = 0, $i -LT $StartIP.Length; $i++){
+    for([Int] $i = 0; $i -LT $StartIP.Length; $i++){
         if($StartIP[$i] -EQ "."){
-           $BitCount++
+           $BitCount++ 
         }
         if ($BitCount -EQ 3) {
             $Subnet = $StartIP
-            [Int] $StartLastBit = $StartIP.Substring($i)
-            break
+            [String] $Temp = $StartIP.Substring($i)
+            $Temp = $Temp.Trim(".")
+            [int] $StartLastBit = [convert]::ToInt32($Temp, 10)
+            $BitCount = 0
+                break
         }
     }
 
-    for([Int] $j = 0, $j -LT $EndIP.Length; $j++){
+    for([Int] $j = 0; $j -LT $EndIP.Length; $j++){
         if($EndIP[$j] -EQ "."){
-           $BitCount++
+           $BitCount++ 
         }
         if ($BitCount -EQ 3) {
-            [Int] $EndLastBit = $EndIP.Substring($j)
-            $BitCount = 0
-            break
+            [String] $Temp = $EndIP.Substring($j)
+            $Temp = $Temp.Trim(".")
+            [int] $EndLastBit = [convert]::ToInt32($Temp, 10)
+                break
         }
     }
 
@@ -51,13 +58,10 @@ function Get-IPs {
                 break
             }
         }
-
+    
    [String[]] $IPrange = "$($Subnet) $($StartLastBit..$EndLastBit)"
-
+   
    foreach($IP in $IPrange) {
        Test-Connection $IP -Count 1 -Quiet | Where-Object {$_ -EQ "True"}
-            $Properties = @{IPAddress = $IP
-                            Status = 'Connected'
-                           }
    }
 }
