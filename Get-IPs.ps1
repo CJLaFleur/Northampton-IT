02234ec35c00a062a6c6846399cdd620a34b28a8
Connor LaFleur <clafleur@main.city.northampton.ma.us>
2017-05-24 15:11:09 -0400

    Mostly finished Get-IPs. Still needs finishing touches to make it easily compatible with Get-Hostnames.

--- Get-IPs.ps1
+++ Get-IPs.ps1
@@ -1,5 +1,5 @@
 function Get-IPs {
-    
+
     [cmdletbinding()]
     Param(
 
@@ -9,59 +9,79 @@ function Get-IPs {
                    ValueFromPipelineByPropertyName = $True,
                    HelpMessage ="Specify the first IP in the range(s).")]
         [Alias('Start')]
-        [String] $StartIP,
+        [String]$StartIP,
 
         [Parameter(Mandatory = $True,
                    Position =1,
                    HelpMessage ="Enter the last IP in the range(s).")]
         [Alias('End')]
-        [String] $EndIP,
+        [String]$EndIP,
 
         [Parameter(Mandatory = $False,
                   HelpMessage ="Enter the subnets you want to scan.")]
-        
-        #This is an array of strings intended to store as many subnets as the user wishes. It is named Multiple so it is clear as a parameter.
-        [String[]] $Multiple 
-    )
-    [Int] $BitCount = 0
-    [String] $Subnet
 
-    for([Int] $i = 0; $i -LT $StartIP.Length; $i++){
+        #This is an array of strings intended to store as many subnets as the user wishes. It is named Multiple so it is clear as a parameter.
+        [String[]]$Multiple
+    )
+    [Int]$Count = 0
+    [Int]$BitCount = 0
+    [String]$Subnet
+    [int]$StartLastBit
+    [int]$EndLastBit
+    [System.Net.IPAddress[]]$IPRange
+
+    Clear-Host
+
+    for([Int]$i = 0; $i -LT $StartIP.Length; $i++){
         if($StartIP[$i] -EQ "."){
-           $BitCount++ 
+           $BitCount++
         }
         if ($BitCount -EQ 3) {
             $Subnet = $StartIP
             [String] $Temp = $StartIP.Substring($i)
             $Temp = $Temp.Trim(".")
-            [int] $StartLastBit = [convert]::ToInt32($Temp, 10)
+            $StartLastBit = [convert]::ToInt32($Temp, 10)
             $BitCount = 0
                 break
         }
     }
 
-    for([Int] $j = 0; $j -LT $EndIP.Length; $j++){
+    for([Int]$j = 0; $j -LT $EndIP.Length; $j++){
         if($EndIP[$j] -EQ "."){
-           $BitCount++ 
+           $BitCount++
         }
         if ($BitCount -EQ 3) {
             [String] $Temp = $EndIP.Substring($j)
             $Temp = $Temp.Trim(".")
-            [int] $EndLastBit = [convert]::ToInt32($Temp, 10)
+            $EndLastBit = [convert]::ToInt32($Temp, 10)
+            $BitCount = 0
                 break
         }
     }
 
-    for([Int] $k = $Subnet.Length; $k -GE 0; $k--){
-            $Subnet = $Subnet -Replace ".$"
-            if($Subnet[$k] -EQ "."){
-                break
-            }
+    for([Int]$k = 0; $k -LT $Subnet.Length; $k++){
+        if($Subnet[$k] -EQ "."){
+           $BitCount++
         }
-    
-   [String[]] $IPrange = "$($Subnet) $($StartLastBit..$EndLastBit)"
-   
-   foreach($IP in $IPrange) {
-       Test-Connection $IP -Count 1 -Quiet | Where-Object {$_ -EQ "True"}
+        if ($BitCount -EQ 3) {
+            [String] $Temp = $Subnet.Substring(0, $k)
+            $Subnet = $Temp + "."
+            $BitCount = 0
+                break
+        }
+    }
+   while($StartLastBit -LE $EndLastBit){
+        $IPRange += @($Subnet + $StartLastBit)
+        $StartLastBit++
    }
+
+  foreach($IP in $IPRange){
+          Test-Connection $IP -Count 1 -Quiet | Where-Object {$_ -EQ "True"}
+
+          $Properties = @{IPAddress = $IP
+                          Status = 'Connected'
+                          }
+          $IPData = New-Object -TypeName PSObject -Property $Properties
+          Write-Output $IPData
+        }
 }
