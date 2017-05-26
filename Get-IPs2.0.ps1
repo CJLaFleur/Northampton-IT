7afdd493f08b31ffc3c298d0dfd12e43b8050b81
Connor LaFleur <clafleur@main.city.northampton.ma.us>
2017-05-26 13:41:08 -0400

    Attempting to multithread this to increase performance.

---
+++ Get-IPs2.0.ps1
@@ -0,0 +1,137 @@
+function Get-IPs {
+    #Parameters
+    [cmdletbinding()]
+    Param(
+
+        [Parameter(Mandatory = $True,
+                   Position =0,
+                   ValueFromPipeline = $True,
+                   ValueFromPipelineByPropertyName = $True,
+                   HelpMessage ="Specify the first IP in the range(s).")]
+        [Alias('Start')]
+        [String]$StartIP,
+
+        [Parameter(Mandatory = $True,
+                   Position =1,
+                   HelpMessage ="Enter the last IP in the range(s).")]
+        [Alias('End')]
+        [String]$EndIP
+)
+
+    #Non-parameter variables
+    [Int]$Count = 0
+    [Int]$BitCount = 0
+    [String]$Subnet
+    [int]$StartLastBit
+    [int]$EndLastBit
+    $IPQueue = New-Object System.Collections.Queue
+    $SleepTimer = 200
+    $MaxResultTime = 120
+    $MaxThreads = 20
+
+    Clear-Host
+
+    #Parse First IP Address
+    for([Int]$i = 0; $i -LT $StartIP.Length; $i++){
+        if($StartIP[$i] -EQ "."){
+           $BitCount++
+        }
+        if ($BitCount -EQ 3) {
+            $Subnet = $StartIP
+            [String] $Temp = $StartIP.Substring($i)
+            $Temp = $Temp.Trim(".")
+            $StartLastBit = [convert]::ToInt32($Temp, 10)
+            $BitCount = 0
+                break
+        }
+    }
+
+    #Parse end IP Address
+    for([Int]$j = 0; $j -LT $EndIP.Length; $j++){
+        if($EndIP[$j] -EQ "."){
+           $BitCount++
+        }
+        if ($BitCount -EQ 3) {
+            [String] $Temp = $EndIP.Substring($j)
+            $Temp = $Temp.Trim(".")
+            $EndLastBit = [convert]::ToInt32($Temp, 10)
+            $BitCount = 0
+                break
+        }
+    }
+
+    #Trims $Subnet down to three octets
+    for([Int]$k = 0; $k -LT $Subnet.Length; $k++){
+        if($Subnet[$k] -EQ "."){
+           $BitCount++
+        }
+        if ($BitCount -EQ 3) {
+            [String] $Temp = $Subnet.Substring(0, $k)
+            $Subnet = $Temp + "."
+            $BitCount = 0
+                break
+        }
+    }
+
+   #Initializes the array of the range of IPs.
+   while($StartLastBit -LE $EndLastBit){
+        [String]$Temp = $Subnet + $StartLastBit
+        $IPQueue.Enqueue($Temp)
+        $StartLastBit++
+   }
+
+   $IPQueue.Dequeue()
+
+   <###############################################-Multithreader Function-#####################################################
+   function Multithreader(){
+
+        BEGIN{
+            $SessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
+            $SessionState.Variables.Add(
+            (New-Object System.Management.Automation.Runspaces.SessionStateVariableEntry('IPRange', $IPRange, 'Range of IP addresses')))
+
+            $RunspacePool = [RunspaceFactory]::CreateRunspacePool(1, 20, $SessionState, $Host)
+            $RunspacePool.BeginOpen()
+
+            $Controller = [PowerShell]::Create()
+            $Controller.RunspacePool = $RunspacePool
+
+
+        }
+
+        PROCESS{
+            $Controller.AddScript({Scan-Network})
+        }
+   }
+
+
+
+
+
+
+
+   #############################################-End Multithreader-############################################################>
+
+   #Iterates through each IP in the array and runs Test-Connection against them.
+
+
+   <#function Scan-Network {
+
+   foreach($IP in $IPRange){
+
+          [Bool]$IsConnected = Test-Connection $IP -Count 1 -Quiet -BufferSize 1 -TimeToLive 1 | Where-Object {$_ -EQ "True"}
+
+          if($IsConnected -EQ "True"){
+          $Properties = @{IPAddress = $IP
+                          Status = 'Connected'
+                          }
+          $IPData = New-Object -TypeName PSObject -Property $Properties
+          Write-Output $IPData
+          }
+          else{
+            Start-Sleep -Milliseconds 1
+          }
+        }
+    }
+    Scan-Network#>
+}
